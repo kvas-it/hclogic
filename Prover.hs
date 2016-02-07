0612e4ea843ecc78570d481b091cdc680b4dcd27
Vasily Kuznetsov <kvas.it@gmail.com>
2016-02-07 19:16:44 +0100

    Define the proof type.

--- Prover.hs
+++ Prover.hs
@@ -7,14 +7,31 @@ data Formula =
     Conj Formula Formula |
     Disj Formula Formula |
     Impl Formula Formula |
-    Prns Formula
+    PrnF Formula
 
 instance Show Formula where
     show (Atom a) = a
     show (Conj a b) = show a ++ " ^ " ++ show b
     show (Disj a b) = show a ++ " v " ++ show b
     show (Impl a b) = show a ++ " => " ++ show b
-    show (Prns a) = "(" ++ show a ++ ")"
+    show (PrnF a) = "(" ++ show a ++ ")"
 
-main =
-    putStrLn $ show $ Impl (Prns (Conj (Atom "A") (Atom "B"))) (Atom "C")
+data Proof =
+    Var String |
+    Appl Proof Proof |
+    Lmbd String (Maybe Formula) Proof |
+    PrnP Proof
+
+instance Show Proof where
+    show (Var a) = a
+    show (Appl a b) = show a ++ show b
+    show (Lmbd x (Just f) p) = "(\\" ++ x ++ " : " ++ show f ++ "). " ++ show p
+    show (Lmbd x Nothing p) = "\\" ++ x ++ ". " ++ show p
+    show (PrnP p) = "(" ++ show p ++ ")"
+
+main = do
+    putStrLn $ show $ Impl (PrnF (Conj (Atom "A") (Atom "B"))) (Atom "C")
+    putStrLn $ show $ Lmbd "x" (Just (Conj (Atom "A") (Atom "B")))
+        (PrnP (Appl (Var "f") (Var "x")))
+    putStrLn $ show $ Lmbd "x" Nothing $ Lmbd "y" Nothing $
+        Appl (Appl (Var "f") (Var "x")) (Var "y")
