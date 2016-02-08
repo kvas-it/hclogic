67ec13f2056e2ad64962bd5b80e11e664317a86c
Vasily Kuznetsov <kvas.it@gmail.com>
2016-02-08 15:41:33 +0100

    Implement proving formulas that we are given proves for.

--- Prover.hs
+++ Prover.hs
@@ -2,12 +2,15 @@
 -- Generate constructive proofs for formulas.
 --
 
+import qualified Data.Map as Map
+
 data Formula =
     Atom String |
     Conj Formula Formula |
     Disj Formula Formula |
     Impl Formula Formula |
     PrnF Formula
+  deriving (Eq, Ord)
 
 instance Show Formula where
     show (Atom a) = a
@@ -21,17 +24,31 @@ data Proof =
     Appl Proof Proof |
     Lmbd String (Maybe Formula) Proof |
     PrnP Proof
+  deriving (Eq, Ord)
 
 instance Show Proof where
     show (Var a) = a
-    show (Appl a b) = show a ++ show b
+    show (Appl a b) = show a ++ " " ++ show b
     show (Lmbd x (Just f) p) = "(\\" ++ x ++ " : " ++ show f ++ "). " ++ show p
     show (Lmbd x Nothing p) = "\\" ++ x ++ ". " ++ show p
     show (PrnP p) = "(" ++ show p ++ ")"
 
+-- Knowledge base.
+type KB = Map.Map Formula Proof
+
+prove :: KB -> Formula -> Maybe Proof
+prove kb f
+    | Map.member f kb  = Just $ kb Map.! f
+    | otherwise        = Nothing
+
+premise = Conj (Atom "A") (Atom "B")
+conclusion = Impl (Atom "A") (Atom "B")
+f1 = Impl (PrnF premise) conclusion
+p1 = Lmbd "x" (Just premise) $ Lmbd "y" (Just (Atom "A")) $ Appl (Var "snd") (Var "x")
+
+kb1 = Map.fromList [(f1, p1)]
+
 main = do
-    putStrLn $ show $ Impl (PrnF (Conj (Atom "A") (Atom "B"))) (Atom "C")
-    putStrLn $ show $ Lmbd "x" (Just (Conj (Atom "A") (Atom "B")))
-        (PrnP (Appl (Var "f") (Var "x")))
-    putStrLn $ show $ Lmbd "x" Nothing $ Lmbd "y" Nothing $
-        Appl (Appl (Var "f") (Var "x")) (Var "y")
+    putStrLn $ show f1
+    putStrLn $ show p1
+    putStrLn $ show $ prove kb1 f1
